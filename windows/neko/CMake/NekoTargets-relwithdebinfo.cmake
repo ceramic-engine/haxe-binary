10ff96ceee4507527f965bb970470616a4884659
Jeremy Faivre <contact@jeremyfa.com>
2024-12-24 01:16:25 +0100

    Ensure neko 2.4.0 is on all platforms, add arm64 linux

--- windows/neko/CMake/NekoTargets-relwithdebinfo.cmake
+++ windows/neko/CMake/NekoTargets-relwithdebinfo.cmake
@@ -11,8 +11,8 @@ set_target_properties(nekovm PROPERTIES
   IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/./neko.exe"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS nekovm )
-list(APPEND _IMPORT_CHECK_FILES_FOR_nekovm "${_IMPORT_PREFIX}/./neko.exe" )
+list(APPEND _cmake_import_check_targets nekovm )
+list(APPEND _cmake_import_check_files_for_nekovm "${_IMPORT_PREFIX}/./neko.exe" )
 
 # Import target "nekoc" for configuration "RelWithDebInfo"
 set_property(TARGET nekoc APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
@@ -20,8 +20,8 @@ set_target_properties(nekoc PROPERTIES
   IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/./nekoc.exe"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS nekoc )
-list(APPEND _IMPORT_CHECK_FILES_FOR_nekoc "${_IMPORT_PREFIX}/./nekoc.exe" )
+list(APPEND _cmake_import_check_targets nekoc )
+list(APPEND _cmake_import_check_files_for_nekoc "${_IMPORT_PREFIX}/./nekoc.exe" )
 
 # Import target "nekotools" for configuration "RelWithDebInfo"
 set_property(TARGET nekotools APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
@@ -29,8 +29,8 @@ set_target_properties(nekotools PROPERTIES
   IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/./nekotools.exe"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS nekotools )
-list(APPEND _IMPORT_CHECK_FILES_FOR_nekotools "${_IMPORT_PREFIX}/./nekotools.exe" )
+list(APPEND _cmake_import_check_targets nekotools )
+list(APPEND _cmake_import_check_files_for_nekotools "${_IMPORT_PREFIX}/./nekotools.exe" )
 
 # Import target "libneko" for configuration "RelWithDebInfo"
 set_property(TARGET libneko APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
@@ -39,8 +39,8 @@ set_target_properties(libneko PROPERTIES
   IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/./neko.dll"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS libneko )
-list(APPEND _IMPORT_CHECK_FILES_FOR_libneko "${_IMPORT_PREFIX}/./neko.lib" "${_IMPORT_PREFIX}/./neko.dll" )
+list(APPEND _cmake_import_check_targets libneko )
+list(APPEND _cmake_import_check_files_for_libneko "${_IMPORT_PREFIX}/./neko.lib" "${_IMPORT_PREFIX}/./neko.dll" )
 
 # Import target "nekoml" for configuration "RelWithDebInfo"
 set_property(TARGET nekoml APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
@@ -48,8 +48,8 @@ set_target_properties(nekoml PROPERTIES
   IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/./nekoml.exe"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS nekoml )
-list(APPEND _IMPORT_CHECK_FILES_FOR_nekoml "${_IMPORT_PREFIX}/./nekoml.exe" )
+list(APPEND _cmake_import_check_targets nekoml )
+list(APPEND _cmake_import_check_files_for_nekoml "${_IMPORT_PREFIX}/./nekoml.exe" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
